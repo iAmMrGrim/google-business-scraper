21c8e2954a09b45855540ed3f18601bd7436db62
iAmMrGrim <118693220+iAmMrGrim@users.noreply.github.com>
2023-12-23 23:31:52 -0700

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,11 @@
+'''
+Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/
+'''
+queries = [
+    {
+        "keyword": "type in search term here",
+        "max_results" : 50,
+    },
+]
+
+number_of_scrapers = 2
